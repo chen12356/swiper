8eb771c73154de31b7779743e70d6d22b72bb523
ccq <you@example.com>
2019-11-27 22:39:06 +0800

    实现短信验证码的发送功能

---
+++ swiper/swiper/config.py
@@ -0,0 +1,14 @@
+"""
+本文件 主要 配置第三方接口的配置信息
+"""
+
+#云之讯短信平台配置
+YZX_SMS_API = 'https://open.ucpaas.com/ol/sms/sendsms'
+YZX_SMA_ARGS = {
+    "sid": "2ff56f07e2d002ab9900777dd4b09edf",
+    "token": "d763718424035afc347cbd3bba3813a2",
+    "appid": "8235102f41ed4603802b05264c59430e",
+    "templateid": "503617",
+    "param": None,  #  发送验证码内容
+    "mobile": None,  # 接收手机号
+}
---
+++ swiper/user/logics.py
@@ -0,0 +1,39 @@
+'''
+本文件主要是编写其他功能的逻辑代码
+'''
+import random
+
+import requests
+from django.core.cache import cache
+
+from swiper.swiper import config
+def get_random_code(length=6):
+    """
+    生成指定长度随机验证码
+    :param length: 默认长度 6 位
+    :return: 返回为 6位验证码  类型：str
+    """
+    return "".join([str(random.randint(0,9)) for i in range(length)])
+
+def send_vcode(phone):
+    """
+    给指定手机发送短信验证码
+    :param phone: 用户手机号
+    :return: 布尔值（True 成功、False 失败）
+    """
+    vcode = get_random_code()
+    args = config.YZX_SMA_ARGS.copy() # 浅拷贝全局的配置，防止多个用户同时操作，导致可变数据类型(这里该字典)发生变化
+    # 添加发送数据
+    args['param'] = vcode
+    #添加接收手机号
+    args['mobile'] = phone
+
+    #请求云之讯平台第三方接口
+    response = requests.post(config.YZX_SMS_API,json=args)
+    if response.status_code == 200:
+        result = response.json()   #转成json数据
+        if result['msg'] == 'ok':
+            # 设置到缓存中：本项目缓存到redis中,过期时间300s，注意：设置的key名尽可能顾名思义
+            cache.set("vcode-%s" % phone,vcode,timeout=300)
+            return True
+    return False
